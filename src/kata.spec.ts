3e4c693c60d772f7c7c2ec877a258dd2a43f1787
joann.jousselin <joann.jousselin@gmail.com>
2022-12-09 14:58:44 +0100

    reset

--- src/kata.spec.ts
+++ src/kata.spec.ts
@@ -1,36 +1,7 @@
-class Game {
-	score = 0;
-	roll(pins: number): void  {
-		this.score += pins;
-	}
-}
+
 
 describe('Kata Bowling', () => {
-	it('gutter game', () => {
-		const game = new Game();
-		for(let i = 0;i<20;i++){
-			game.roll(0)
-		}
-		expect(game.score).toBe(0);
-	});
-
-	it('1 pin for each try', () => {
-		const game = new Game();
-		for(let i = 0;i<20;i++){
-			game.roll(1)
-		}
-		expect(game.score).toBe(20);
-	});
-
-	it('spare case', () => {
-		const game = new Game();
-		game.roll(3);
-		game.roll(7);
-		game.roll(2);
-		game.roll(0);
-		for(let i = 0;i<16;i++){
-			game.roll(0);
-		}
-		expect(game.score).toBe(14);
+	it('todo', () => {
+		expect(0).toBe(0);
 	});
 });
