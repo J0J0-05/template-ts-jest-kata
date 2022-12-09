324000172b55a8d0aeee7716e5a454ac425c86cc
jonatmoy <jonatmoy@gmail.com>
2022-12-09 14:49:45 +0100

    test commit 2

--- src/kata.spec.ts
+++ src/kata.spec.ts
@@ -17,8 +17,20 @@ describe('Kata Bowling', () => {
 	it('1 pin for each try', () => {
 		const game = new Game();
 		for(let i = 0;i<20;i++){
-			game.roll(0)
+			game.roll(1)
 		}
-		expect(game.score).toBe(0);
+		expect(game.score).toBe(20);
+	});
+
+	it('spare case', () => {
+		const game = new Game();
+		game.roll(3);
+		game.roll(7);
+		game.roll(2);
+		game.roll(0);
+		for(let i = 0;i<16;i++){
+			game.roll(0);
+		}
+		expect(game.score).toBe(14);
 	});
 });
