b1971ac1045d05768cc619494bbfc38e3fcff4d0
joann.jousselin <joann.jousselin@gmail.com>
2022-12-13 10:23:32 +0100

    com

--- src/kata.spec.ts
+++ src/kata.spec.ts
@@ -1,27 +1,38 @@
 class Game {
-	score: number = 0;
-	
-	Roll(pinsDown: number) {
-		return this.score += pinsDown;
-	}
+	score = 0;
 
+	roll(pinsDown: number): number {
+		return (this.score += pinsDown);
+	}
 }
 
 describe('Kata Bowling', () => {
+	function rolls(game: Game, pinsDown: number, rolls: number): void {
+		for (let i = 0; i < rolls; i++) {
+			game.roll(pinsDown);
+		}
+	}
+
 	it('Gutter Game', () => {
 		const game = new Game();
-		for(let i = 0; i<20;i++){
-			game.Roll(0)
-		}
+		rolls(game, 0, 20);
 		expect(game.score).toBe(0);
 	});
 
 	it('One pin down on each rolls', () => {
 		const game = new Game();
-		for(let i = 0; i<20;i++){
-			game.Roll(1)
-		}
+		rolls(game, 1, 20);
 		expect(game.score).toBe(20);
 	});
 
+	it('Spare case', () => {
+		const game = new Game();
+		game.roll(3);
+		game.roll(7);
+
+		game.roll(2);
+		rolls(game, 0, 17);
+
+		expect(game.score).toBe(7 + 3 + 2 + 2);
+	});
 });
