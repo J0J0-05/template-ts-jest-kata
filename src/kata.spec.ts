83da26ca77830fa84dc6f8df00074d0a60af2b1d
jonatmoy <jonatmoy@gmail.com>
2022-12-13 10:14:30 +0100

    commit kata 1

--- src/kata.spec.ts
+++ src/kata.spec.ts
@@ -1,7 +1,27 @@
+class Game {
+	score: number = 0;
+	
+	Roll(pinsDown: number) {
+		return this.score += pinsDown;
+	}
 
+}
 
 describe('Kata Bowling', () => {
-	it('todo', () => {
-		expect(0).toBe(0);
+	it('Gutter Game', () => {
+		const game = new Game();
+		for(let i = 0; i<20;i++){
+			game.Roll(0)
+		}
+		expect(game.score).toBe(0);
 	});
+
+	it('One pin down on each rolls', () => {
+		const game = new Game();
+		for(let i = 0; i<20;i++){
+			game.Roll(1)
+		}
+		expect(game.score).toBe(20);
+	});
+
 });
